ebcc8c55977bc4b7cc99b16bd38cc9a3796a7342
acidcor <deadfoxua1337@gmail.com>
2025-09-02 22:02:49 +0200

    implement cache decorator pls style

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cashe_dics = {}
+    def casher(*args) -> Any:
+        if args in cashe_dics:
+            print("Getting from cache")
+            return cashe_dics[*args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cashe_dics[*args] = result
+            return result
+    return casher
